cb1d54944a2e927d72f46f33b30b9224bac809f9
light130 <light13silver@gmail.com>
2019-10-14 18:35:30 +0900

    Make user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#top'
-  devise_for :users
+  devise_for :users, controllers: {
+     sessions: 'users/sessions',
+     passwords: 'users/passwords',
+     registrations: 'users/registrations'
+   }
 end
