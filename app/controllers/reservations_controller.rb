41dae68f3c250c9c958efae7c0c0639aabd10d38
light130 <light13silver@gmail.com>
2019-10-20 23:07:52 +0900

    Add index, show and destroy to reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,23 +1,38 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!, only: [:confirmation, :create]
+  before_action :authenticate_user!, only: [:confirmation, :create, :index, :show, :destroy]
 
   def new
-    @reservation = Reservation.all
-    @reserved_schedule_ids = @reservation.select("schedule_id")
+    @reservations = Reservation.all
+    @reserved_schedule_ids = @reservations.select("schedule_id")
     @schedule_available = Schedule.where.not(id: @reserved_schedule_ids)
   end
 
   def create
     @reservation = Reservation.new(schedule_id: params[:schedule_id], user_id: params[:user_id])
     if @reservation.save
+      flash[:info] = ["予約を完了しました。"]
       redirect_to root_url
     else
-      flash[:errors] = ["予約を完了できませんでした。"]
+      flash[:info] = ["予約を完了できませんでした。"]
       redirect_to root_url
     end
   end
 
   def index
+    @reservations = Reservation.where(user_id: current_user.id)
+    @reserved_schedule_ids = @reservations.select("schedule_id")
+    @schedules = Schedule.where(id: @reserved_schedule_ids)
+  end
+
+  def show
+    @schedule = Schedule.find(params[:id])
+    @reservation = Reservation.find_by(schedule_id: @schedule.id)
+  end
+
+  def destroy
+    Reservation.find(params[:id]).destroy
+    flash[:info] = ["予約を取り消しました。"]
+    redirect_to root_url
   end
 
   def confirmation
